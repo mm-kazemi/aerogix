70467cb2047562d088a2fe75c4667e7445befdff
mm.kazemi <mm.kazemi.work@gmail.com>
2025-11-16 16:29:04 +0330

    [ADD] max width to header

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,16 +1,18 @@
-import {type PropsWithChildren, type ReactElement} from "react";
+import { type PropsWithChildren, type ReactElement } from "react";
 
 import styles from "./button.module.css";
 import clsx from "clsx";
 
-export type Variant =  "solid" | "outline"
+export type Variant = "solid" | "outline";
 
 type Props = PropsWithChildren & {
-    variant: Variant;
+  variant: Variant;
 };
 
-function Button({ children, variant = "solid", }: Props): ReactElement {
-  return <button className={clsx(styles.button, styles[variant])}>{children}</button>;
+function Button({ children, variant = "solid" }: Props): ReactElement {
+  return (
+    <button className={clsx(styles.button, styles[variant])}>{children}</button>
+  );
 }
 
 export default Button;
