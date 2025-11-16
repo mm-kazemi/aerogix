2535d7d1bf89032fc212491996ce04bbbbfdfc74
mm.kazemi <mm.kazemi.work@gmail.com>
2025-11-16 17:48:51 +0330

    [ADD] hero text and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import {Header} from "./components/Header/Header.tsx";
+import Hero from "./components/Hero/hero.tsx";
 
 function App() {
   return (
     <>
         <Header />
-      <main className="App"></main>
+      <main className="App"><Hero/></main>
       <footer></footer>
     </>
   );
---
+++ src/components/Hero/hero.tsx
@@ -0,0 +1,23 @@
+import type { ReactElement } from "react";
+
+import Button from "../Button/button.tsx";
+
+import styles from "./hero.module.css"
+
+function Hero(): ReactElement {
+  return (
+    <div className={styles.hero}>
+      <h1>Effortless Logistics, Every Step of the Way.</h1>
+      <p>
+        Manage your shipments, track in real-time, and get the best rates—all
+        from one platform.
+      </p>
+        <div className={styles.actions}>
+            <Button variant={"solid"}>Get a Free Quote</Button>
+            <Button variant={"outline"}>Learn More</Button>
+        </div>
+    </div>
+  );
+}
+
+export default Hero;
