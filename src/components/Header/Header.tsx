d6dc5e43e363b3d0d832d028ccc431ca62c065de
mm-kazemi <mm.kazemi.work@gmail.com>
2025-11-16 04:07:01 +0330

    [UPDATE] header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import styles from "./Header.module.css";
 export function Header() {
   return (
     <header className={styles.header}>
-      <div className="logo">
+      <div className={styles.logo}>
         <img src={"./public/assets/logos/Logo.svg"} alt={"Aerogix Logo"} />
       </div>
       <nav>
@@ -25,7 +25,7 @@ export function Header() {
           </li>
         </ul>
       </nav>
-      <div className="actio">
+      <div className={styles.action}>
         <button>Log In</button>
         <button>Sign Up</button>
       </div>
